bd18562b9378d1e7bd4575b59861381c381bff61
Peter_Home <someone.on.the.internet000@gmail.com>
2023-02-07 14:54:09 +0530

    misc.x Changed Python version from 3.8 to 3.11
    urls.
    Added extra URL which was not previously working

--- APIAuthentication/urls.py
+++ APIAuthentication/urls.py
@@ -19,5 +19,7 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student-api/<int:pk>',views.StudentApi.as_view()),
+    path('studentapi/<int:pk>/', views.StudentApi.as_view()),
+    path('studentapi/', views.StudentApi.as_view()),
+
 ]
